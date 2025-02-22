2927afa9fb2eafd33f198137702c6279353c07ea
Kim <kimgyudo93@gmail.com>
2025-02-22 18:32:04 +0900

    first commit

---
+++ practice_shooting/android/app/src/main/kotlin/com/example/practice_shooting/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.practice_shooting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ practice_shooting/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:practice_shooting/component/player.dart';
+
+void main() {
+  runApp(GameWidget(game: SpaceShooterGame()));
+}
+
+class Player extends PositionComponent {
+  static final _paint = Paint()..color = Colors.white;
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(size.toRect(), _paint);
+  }
+}
+
+class SpaceShooterGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    add(
+      Player()
+        ..position = size / 2
+        ..width = 50
+        ..height = 100
+        ..anchor = Anchor.center,
+    );
+  }
+}
+
+// class SpaceShooterGame extends FlameGame {
+//
+//   @override
+//   Future<void> onLoad() async{
+//     await super.onLoad();
+//
+//     add(Player()
+//         ..position = size / 2
+//         ..width = 50
+//         ..height = 100
+//         ..anchor = Anchor.center
+//     );
+//   }
+// }
+//
+// void main() {
+//   runApp(GameWidget(game: SpaceShooterGame()));
+// }
